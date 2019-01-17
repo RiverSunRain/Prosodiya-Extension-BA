566b2a480608dd076b3e92aa6f240284fe3b5b27
[Ulli] <[Haku-zubaki@gmx.de]>
2019-01-17 16:12:00 +0100

    First commit
    Hi Benedikt
    
    First commit
    Does it work?

---
+++ Prr_Ext_1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GUILayout.Label("Let's go!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
