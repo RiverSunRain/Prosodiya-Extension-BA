a036a1d91c1e2f945c250714364beddbdb5c62b3
[Ulli] <[Haku-zubaki@gmx.de]>
2019-01-25 13:40:53 +0100

    Update
    Run an Audio File when the exercise starts.

---
+++ Prr_Ext_1/Assets/start_audio_script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start_audio_script : MonoBehaviour
+{
+
+
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+        MusicSource.Play();
+    }
+}
