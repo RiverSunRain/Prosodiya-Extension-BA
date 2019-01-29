b06fad8a75bc1d64c1aef517419ecb426daf4311
[Ulli] <[Haku-zubaki@gmx.de]>
2019-01-29 12:07:06 +0100

    Update
    Now, objects are clickable.

--- Prr_Ext_1/Assets/d_button_script.cs
+++ Prr_Ext_1/Assets/d_button_script.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tw_button_script : MonoBehaviour
+public class d_button_script : MonoBehaviour
 {
     public void OnMouseDown()
     {
-        Destroy (gameObject);
+        Destroy(gameObject);
     }
 }
