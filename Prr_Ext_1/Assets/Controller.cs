66992ea5aeac1fcc1ca8562528163a9c115b9112
[Ulli] <[Haku-zubaki@gmx.de]>
2019-02-14 14:04:14 +0100

    words and lists
    lists of lists for all words

--- Prr_Ext_1/Assets/Controller.cs
+++ Prr_Ext_1/Assets/Controller.cs
@@ -9,24 +9,55 @@ public class Controller : MonoBehaviourSingleton<Controller>
 
     public GameObject CloudWrapper;
     public GameObject CloudPrefabs;
+
+    List<List<Word>> mainList = new List<List<Word>>();
+
     public List<Word> WordList;
+    public List<Word> WordList2;
+    public List<Word> WordList3;
+    public List<Word> WordList4;
+    public List<Word> WordList5;
+    public List<Word> WordList6;
+    public List<Word> WordList7;
+    public List<Word> WordList8;
+    public List<Word> WordList9;
+    public List<Word> WordList10;
+
     public List<Vector2> AllowedPositions;
 
-    
-
-   
     //referenz zum audio
     public start_audio_script sas;
 
     void Start()
     {
+        mainList.Add(WordList);
+        mainList.Add(WordList2);
+        mainList.Add(WordList3);
+        mainList.Add(WordList4);
+        mainList.Add(WordList5);
+        mainList.Add(WordList6);
+        mainList.Add(WordList7);
+        mainList.Add(WordList8);
+        mainList.Add(WordList9);
+        mainList.Add(WordList10);
+
         PrepareScene();
     }
 
     public void PrepareScene()
     {
-        PrepareClouds();
+        
+        /*
+        for (var i = 0; i < 10; i++)
+        {
+            PrepareClouds(mainList[i]); // Interation through al sublist, doesn't work :(
+        }
+        **/
+
+        PrepareClouds(mainList[0]);
+
         StartGame();
+        
     }
 
     public void StartGame()
@@ -34,11 +65,11 @@ public class Controller : MonoBehaviourSingleton<Controller>
         sas.PlayAudioClip();
     }
 
-    public void PrepareClouds()
+    public void PrepareClouds(List<Word> subList)
     {
         int i = 0;
 
-        foreach (var word in WordList)
+        foreach (var word in subList)
         {
             //word erstellen
             var x = Instantiate(CloudPrefabs, CloudWrapper.transform);
@@ -50,6 +81,7 @@ public class Controller : MonoBehaviourSingleton<Controller>
             x.GetComponent<RectTransform>().anchoredPosition = randomPos;
             AllowedPositions.Remove(randomPos);
         }
+       
     }
     
     public void CleanupScene()
@@ -59,7 +91,7 @@ public class Controller : MonoBehaviourSingleton<Controller>
             Destroy(child.gameObject);
         }
     }
-
+    
     public void ChangeScene()
     {
         var SceneNumber = 2;
