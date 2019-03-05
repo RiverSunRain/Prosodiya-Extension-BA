d9e059d75274cc7a7d77e50d824139b1f3fbd1a4
[Ulli] <[Haku-zubaki@gmx.de]>
2019-03-05 11:39:51 +0100

    Subject number input added
    Subject number input added with an additional scene.

---
+++ Prr_Ext_1/Assets/CharacterCreator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+public class CharacterCreator : MonoBehaviour {
+
+    public InputField SubjectNumberField;
+    private string number;
+
+    public void OnSubmit() {
+
+        number = SubjectNumberField.text;
+        Debug.Log("Subject number: " + number);
+        SceneManager.LoadScene("Scene_1");
+    }
+}
